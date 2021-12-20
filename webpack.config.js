4d23c48edb3daa1101f17082a232e66f212ef30f
Céline Foucart <celinefoucart@yahoo.fr>
2021-12-20 15:09:04 +0100

    page about

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: "./src/script.js",
     output: {
         path: path.resolve(__dirname, 'dist/js'),
         filename: "bundle.js"
     },
-    devtool: "source-map",
+    // devtool: "source-map",
     devServer: {
         static: {
             directory: path.join(__dirname, 'dist'),
@@ -15,5 +15,25 @@ module.exports = {
         compress: true,
         port: 9000,
         open: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: [/node_modules/, /tests/],
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { 
+                                targets: "defaults",
+                                useBuiltIns: 'usage',
+                                corejs: 3 
+                            }]
+                        ]
+                    }
+                }
+            }
+        ]
     }
 }
